990c67d06550267807525f99cad1c6f9eaa36d19
Kenichi Sakamoto <kenichi.vshawaii@gmail.com>
2021-07-01 13:03:27 -1000

    test

---
+++ ExampleApplication/ExampleApplication/Data/SchoolContext.cs
@@ -0,0 +1,17 @@
+﻿using ExampleApplication.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ExampleApplication.Data
+{
+    public class SchoolContext : DbContext
+    {
+        public SchoolContext(DbContextOptions<SchoolContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Course> Courses { get; set; }
+        public DbSet<Enrollment> Enrollments { get; set; }
+        public DbSet<Student> Students { get; set; }
+
+    }
+}
---
+++ ExampleApplication/ExampleApplication/Models/Course.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ExampleApplication.Models
+{
+    public class Course
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        public int CourseID { get; set; }
+        public string Title { get; set; }
+        public int Credits { get; set; }
+        public ICollection<Enrollment> Enrollements { get; set; }
+    }
+}
